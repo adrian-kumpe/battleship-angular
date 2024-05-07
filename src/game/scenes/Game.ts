24a12cd1ba8d1ead97738b330d6c1a26b9af795e
Jacques-Maurice <Jacques-Maurice.Walther@mailbox.tu-dresden.de>
2024-05-07 15:10:41 +0200

    added short explaination text

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -144,6 +144,17 @@ export class Game extends Scene {
       color: '#000000',
     });
 
+    this.gameText = this.add.text(
+      offsetX,
+      offsetY + 435,
+      'There are 3 ships to hit\n    aircraft-carrier with size: 5\n    cruiser with size: 3\n    destroyer with size: 2',
+      {
+        fontFamily: 'Arial Black',
+        fontSize: 24,
+        color: '#000000',
+      },
+    );
+
     offsetX = 550;
 
     for (let row = 0; row < gridSize; row++) {
@@ -163,6 +174,12 @@ export class Game extends Scene {
     this.gameText = this.add.text(offsetX - 25, offsetY + 5, '1 \n\n2 \n\n3 \n\n4 \n\n5 \n\n6 \n\n7 \n\n8', {
       fontFamily: 'Arial Black',
       fontSize: 23,
+      color: '#000000',
+    });
+
+    this.gameText = this.add.text(offsetX, offsetY + 435, "The opponent tries to guess \nyour ships' positions.", {
+      fontFamily: 'Arial Black',
+      fontSize: 24,
       color: '#000000',
     });
   }
